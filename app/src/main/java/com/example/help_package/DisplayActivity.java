77308c21e50071281f26336605826afecc915726
Batman511 <58048279+Batman511@users.noreply.github.com>
2020-05-04 21:36:14 +0300

    рабочий первый запрос + учет пустого файла + 2 кнопки + стиль

---
+++ app/src/main/java/com/example/help_package/DisplayActivity.java
@@ -0,0 +1,57 @@
+package com.example.help_package;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DisplayActivity extends AppCompatActivity {
+    private WebView browser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+
+        getActionBar().setTitle("Ваши приложения");
+        getActionBar().setIcon(R.drawable.wolf1);
+        getActionBar().setBackgroundDrawable(new
+                ColorDrawable(Color.parseColor("#483D8B")));
+
+        WebView browser = (WebView) findViewById(R.id.webBrowser);
+        browser.loadUrl("https://amarketproject.000webhostapp.com/uploads/?C=N;O=A");
+        browser.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (browser.canGoBack())
+            browser.goBack();
+        else
+            super.onBackPressed();
+    }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.display_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.close_website:
+                Intent intent = new Intent(DisplayActivity.this, MainActivity.class);
+                startActivity(intent);
+                //("происходит переход в приложение");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    } */
+}
